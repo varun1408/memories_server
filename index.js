0d6e95d14e5cbad042fa9a6ba10b51f808a2c54f
varun <varun@airoxa.in>
2021-03-17 12:16:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser' ;
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit :"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit :"30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes); 
+app.use('/users', userRoutes);
+
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true})
+    .then(()=> app.listen(port, ()=> console.log(`server running on port : ${port} `)))
+    .catch((error) => console.log(error.message));
+
+    mongoose.set('useFindAndModify', false);
